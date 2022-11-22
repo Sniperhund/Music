37b572d29834d92faaa8ce31c145a16542704249
Lucas Täkker <lucastakker@gmail.com>
2022-11-22 12:24:40 +0100

    initial commit

---
+++ components/full-components/Sidepanel.tsx
@@ -0,0 +1,8 @@
+export default function Sidepanel() {
+    return <div className="overflow-hidden flex h-screen">
+        <div className="h-screen w-64 bg-[#252526]">
+            <a><h2 className="text-white m-2 text-xl">Music Program</h2></a>
+        </div>
+        <div className="h-screen w-1 float-left bg-[#383839]" />
+    </div>
+}
