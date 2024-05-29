e50c42d98e9aa4d1b367a4076236acce1d8fe420
Lucas Täkker <50625016+Sniperhund@users.noreply.github.com>
2024-05-29 10:19:47 +0200

    Updated auth pages use formik

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -13,34 +13,37 @@ import {
 	Heading,
 	Input,
 	Link,
-	useColorMode,
 	useToast,
 } from "@chakra-ui/react"
+import { useFormik } from "formik"
 import { useRouter } from "next/router"
-import { useEffect, useState } from "react"
+import { ReactElement } from "react"
 
 export default function Signin() {
 	const toast = useToast()
 	const router = useRouter()
 
-	const [email, setEmail] = useState("")
-	const [password, setPassword] = useState("")
+	const formik = useFormik({
+		initialValues: {
+			email: "",
+			password: "",
+		},
+		onSubmit: async (values) => {
+			const result = await signin(values.email, values.password)
 
-	async function signIn() {
-		const result = await signin(email, password)
+			if (result?.error) {
+				console.log(result?.result.response.data.message)
+				toast({
+					title: result?.result.response.data.message,
+					status: "error",
+					isClosable: true,
+				})
+				return
+			}
 
-		if (result?.error) {
-			console.log(result?.result.response.data.message)
-			toast({
-				title: result?.result.response.data.message,
-				status: "error",
-				isClosable: true,
-			})
-			return
-		}
-
-		router.push("/")
-	}
+			router.push("/")
+		},
+	})
 
 	return (
 		<Center className="h-screen">
@@ -51,26 +54,32 @@ export default function Signin() {
 					</Heading>
 				</CardHeader>
 				<Divider opacity="0.2" />
-				<CardBody className="flex flex-col gap-4">
-					<FormControl>
-						<FormLabel>Email address</FormLabel>
-						<Input
-							type="email"
-							value={email}
-							onChange={(e) => setEmail(e.target.value)}
-						/>
-					</FormControl>
+				<CardBody>
+					<form
+						onSubmit={formik.handleSubmit}
+						className="flex flex-col gap-4">
+						<FormControl isRequired>
+							<FormLabel>Email address</FormLabel>
+							<Input
+								name="email"
+								type="email"
+								value={formik.values.email}
+								onChange={formik.handleChange}
+							/>
+						</FormControl>
 
-					<FormControl>
-						<FormLabel>Password</FormLabel>
-						<Input
-							type="password"
-							value={password}
-							onChange={(e) => setPassword(e.target.value)}
-						/>
-					</FormControl>
+						<FormControl isRequired>
+							<FormLabel>Password</FormLabel>
+							<Input
+								name="password"
+								type="password"
+								value={formik.values.password}
+								onChange={formik.handleChange}
+							/>
+						</FormControl>
 
-					<Button onClick={signIn}>Sign in</Button>
+						<Button type="submit">Sign in</Button>
+					</form>
 				</CardBody>
 				<Divider opacity="0.2" />
 				<CardFooter>
@@ -79,4 +88,8 @@ export default function Signin() {
 			</Card>
 		</Center>
 	)
+}
+
+Signin.getLayout = function getLayout(page: ReactElement) {
+	return { page }
 }
--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -1,8 +1,110 @@
 import signup from "@/util/signup"
-import { Button, useColorMode } from "@chakra-ui/react"
+import {
+	Button,
+	Card,
+	CardBody,
+	CardFooter,
+	CardHeader,
+	Center,
+	Divider,
+	FormControl,
+	FormHelperText,
+	FormLabel,
+	Heading,
+	Input,
+	Link,
+	useToast,
+} from "@chakra-ui/react"
+import { useFormik } from "formik"
 import { useRouter } from "next/router"
-import { useEffect } from "react"
+import { ReactElement } from "react"
 
-export default function Signin() {
-	return <Button>Hello</Button>
+export default function Signup() {
+	const toast = useToast()
+	const router = useRouter()
+
+	const formik = useFormik({
+		initialValues: {
+			name: "",
+			email: "",
+			password: "",
+		},
+		onSubmit: async (values) => {
+			const result = await signup(
+				values.email,
+				values.password,
+				values.name
+			)
+
+			if (result?.error) {
+				console.log(result?.result.response.data.message)
+				toast({
+					title: result?.result.response.data.message,
+					status: "error",
+					isClosable: true,
+				})
+				return
+			}
+
+			router.push("/")
+		},
+	})
+
+	return (
+		<Center className="h-screen">
+			<Card maxW="lg" className="w-full">
+				<CardHeader>
+					<Heading size="lg" className="text-center">
+						Welcome back!
+					</Heading>
+				</CardHeader>
+				<Divider opacity="0.2" />
+				<CardBody>
+					<form
+						onSubmit={formik.handleSubmit}
+						className="flex flex-col gap-4">
+						<FormControl isRequired>
+							<FormLabel>Full name</FormLabel>
+							<Input
+								name="name"
+								type="text"
+								value={formik.values.name}
+								onChange={formik.handleChange}
+							/>
+						</FormControl>
+
+						<FormControl isRequired>
+							<FormLabel>Email address</FormLabel>
+							<Input
+								name="email"
+								type="email"
+								value={formik.values.email}
+								onChange={formik.handleChange}
+							/>
+						</FormControl>
+
+						<FormControl isRequired>
+							<FormLabel>Password</FormLabel>
+							<Input
+								name="password"
+								type="password"
+								value={formik.values.password}
+								onChange={formik.handleChange}
+							/>
+						</FormControl>
+
+						<Button type="submit">Sign up</Button>
+					</form>
+				</CardBody>
+				<Divider opacity="0.2" />
+				<CardFooter>
+					<Link href="/auth/signin">Sign in instead?</Link>
+				</CardFooter>
+			</Card>
+		</Center>
+	)
+}
+
+Signup.getLayout = function getLayout(page: ReactElement) {
+	return { page }
 }
