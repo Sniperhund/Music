5b87cb1516e51ebe59f43da35641ed7e21ca813c
Lucas Täkker <50625016+Sniperhund@users.noreply.github.com>
2024-05-29 10:19:27 +0200

    Added middleware to force users to log in

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from "next/server"
+
+export default function middleware(request: NextRequest) {
+	if (request.nextUrl.pathname.startsWith("/_next/"))
+		return NextResponse.next()
+
+	if (
+		!request.nextUrl.pathname.startsWith("/auth/") &&
+		!request.cookies.has("access_token")
+	)
+		return NextResponse.redirect(new URL("/auth/signin", request.url))
+
+	return NextResponse.next()
+}
