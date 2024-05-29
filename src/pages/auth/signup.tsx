3b2f60c436ffc6602e1fd1537d7db977be3e5d0e
Lucas Täkker <lucas@lucasskt.dk>
2024-05-29 07:47:45 +0200

    Signup page

---
+++ src/pages/auth/signup.tsx
@@ -0,0 +1,8 @@
+import signup from "@/util/signup"
+import { Button, useColorMode } from "@chakra-ui/react"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+
+export default function Signin() {
+	return <Button>Hello</Button>
+}
