13f0783c1dd37eb3531dacd924f63489ced216cf
Lucas Täkker <lucastakker@gmail.com>
2024-07-27 02:50:25 +0200

    feat: Update Genres page to include pagination and genre management functionality

--- src/pages/admin/manage/genres.tsx
+++ src/pages/admin/manage/genres.tsx
@@ -1,8 +1,283 @@
+import ArtistName from "@/components/ArtistName"
 import AdminLayout from "@/components/layouts/Admin"
-import { ReactElement } from "react"
+import useAPI from "@/util/useAPI"
+import {
+	Button,
+	TableContainer,
+	Select,
+	Table,
+	Thead,
+	Tr,
+	Th,
+	Tbody,
+	Td,
+	Menu,
+	MenuButton,
+	MenuList,
+	MenuItem,
+	Modal,
+	ModalOverlay,
+	ModalContent,
+	ModalHeader,
+	ModalCloseButton,
+	ModalBody,
+	ModalFooter,
+	useDisclosure,
+	Input,
+	FormLabel,
+	FormControl,
+	FormHelperText,
+	useToast,
+} from "@chakra-ui/react"
+import { Select as MultiSelect } from "chakra-react-select"
+import { NoSSR } from "@kwooshung/react-no-ssr"
+import { ChevronDownIcon } from "lucide-react"
+import Link from "next/link"
+import { useRouter } from "next/router"
+import { ReactElement, useEffect, useState } from "react"
+import FileUpload from "@/components/common/FileUpload"
 
 export default function Genres() {
-	return <>Admin DASHBOARD</>
+	const router = useRouter()
+	const toast = useToast()
+	const { isOpen, onOpen, onClose } = useDisclosure()
+
+	function setLimit(limit: number) {
+		if (!limit) return
+
+		const page = parseInt(router.query.page as string) || 1
+
+		router.push(
+			{
+				pathname: "/admin/manage/genres",
+				query: { limit, page },
+			},
+			undefined,
+			{ shallow: true }
+		)
+	}
+
+	function nextPage() {
+		const page = parseInt(router.query.page as string) || 1
+		const limit = parseInt(router.query.limit as string) || 10
+
+		router.push(
+			{
+				pathname: "/admin/manage/genres",
+				query: { limit, page: page + 1 },
+			},
+			undefined,
+			{ shallow: true }
+		)
+	}
+
+	function prevPage() {
+		const page = parseInt(router.query.page as string) || 1
+		const limit = parseInt(router.query.limit as string) || 10
+
+		if (page <= 1) return
+
+		router.push(
+			{
+				pathname: "/admin/manage/genres",
+				query: { limit, page: page - 1 },
+			},
+			undefined,
+			{ shallow: true }
+		)
+	}
+
+	const [tableData, setTableData] = useState<any>()
+	const [activeGenre, setActiveGenre] = useState<any>()
+
+	async function fetchData() {
+		const page = parseInt(router.query.page as string) || 1
+		const limit = parseInt(router.query.limit as string) || 10
+
+		const result: any = await useAPI(
+			`/all/genres?offset=${(page - 1) * limit}&limit=${limit}`
+		)
+
+		if (result.status)
+			return alert(`Failed to fetch data: ${result.message}`)
+
+		setTableData(result)
+	}
+
+	useEffect(() => {
+		fetchData()
+	}, [router.query.page, router.query.limit])
+
+	const [genreName, setGenreName] = useState("")
+
+	async function submit(event: any) {
+		event.preventDefault()
+
+		if (!genreName) return
+
+		let formData = new FormData()
+
+		if (genreName) formData.append("name", genreName)
+
+		const result: any = await useAPI(`/admin/genre?id=${activeGenre._id}`, {
+			method: "PUT",
+			data: formData,
+			headers: {
+				"Content-Type": "multipart/form-data",
+			},
+		})
+
+		if (result._id)
+			toast({
+				status: "success",
+				title: "Genre changed successfully",
+			})
+		else
+			toast({
+				title: "An error happened",
+				description: result.message,
+				status: "error",
+			})
+
+		fetchData()
+	}
+
+	return (
+		<>
+			<h1>Manage genres</h1>
+
+			<section className="flex justify-between my-4">
+				<Button onClick={() => prevPage()}>Previous page</Button>
+				<Select
+					maxW={72}
+					value={router.query.limit || 10}
+					onChange={(e) => setLimit(parseInt(e.target.value))}>
+					<option value="10" selected>
+						10
+					</option>
+					<option value="25">25</option>
+					<option value="50">50</option>
+				</Select>
+				<Button onClick={() => nextPage()}>Next page</Button>
+			</section>
+
+			<TableContainer>
+				<Table>
+					<Thead>
+						<Tr>
+							<Th>id</Th>
+							<Th>name</Th>
+							<Th className="w-12">action</Th>
+						</Tr>
+					</Thead>
+					<Tbody>
+						{tableData?.map((genre: any) => (
+							<Tr key={genre._id}>
+								<Td>{genre._id}</Td>
+								<Td>{genre.name}</Td>
+								<Td>
+									<Menu>
+										<MenuButton
+											as={Button}
+											rightIcon={<ChevronDownIcon />}>
+											Actions
+										</MenuButton>
+										<MenuList>
+											<MenuItem
+												onClick={() => {
+													setActiveGenre(genre)
+													setGenreName(genre.name)
+													onOpen()
+												}}>
+												Edit details
+											</MenuItem>
+											<MenuItem
+												onClick={async () => {
+													if (
+														confirm(
+															"Are you sure you want to delete this genre?"
+														)
+													) {
+														const result: any =
+															await useAPI(
+																`/admin/genre?id=${genre._id}`,
+																{
+																	method: "DELETE",
+																}
+															)
+
+														if (result?.data.status)
+															return alert(
+																`Failed to delete genre: ${result.data.message}`
+															)
+													}
+
+													fetchData()
+												}}>
+												Delete
+											</MenuItem>
+										</MenuList>
+									</Menu>
+								</Td>
+							</Tr>
+						))}
+					</Tbody>
+				</Table>
+			</TableContainer>
+
+			<section className="flex justify-between my-4">
+				<Button onClick={() => prevPage()}>Previous page</Button>
+				<Select
+					maxW={72}
+					value={router.query.limit || 10}
+					onChange={(e) => setLimit(parseInt(e.target.value))}>
+					<option value="10" selected>
+						10
+					</option>
+					<option value="25">25</option>
+					<option value="50">50</option>
+				</Select>
+				<Button onClick={() => nextPage()}>Next page</Button>
+			</section>
+
+			<Modal isOpen={isOpen} onClose={onClose} size="xl">
+				<ModalOverlay />
+				<ModalContent>
+					<ModalHeader>{activeGenre?.name}</ModalHeader>
+					<ModalCloseButton />
+					<ModalBody>
+						<form
+							onSubmit={submit}
+							className="space-y-4 max-w-2xl mt-4">
+							<FormControl
+								isInvalid={genreName === ""}
+								isRequired>
+								<FormLabel>Genre Name</FormLabel>
+								<Input
+									onChange={(e) =>
+										setGenreName(e.target.value)
+									}
+									value={genreName}
+								/>
+							</FormControl>
+
+							<div>
+								<Button
+									className="mt-6"
+									type="submit"
+									w="full"
+									onClick={() => onClose()}>
+									Submit
+								</Button>
+							</div>
+						</form>
+					</ModalBody>
+
+					<ModalFooter></ModalFooter>
+				</ModalContent>
+			</Modal>
+		</>
+	)
 }
 
 Genres.getLayout = function getLayout(page: ReactElement) {
