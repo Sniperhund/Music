4b3ccfc6f9f4806b9b7cc17f2ffa41769dc4620b
Lucas Täkker <lucastakker@gmail.com>
2024-08-12 22:06:15 +0200

    refactor: Update Albums page to fetch and display albums from API

--- src/pages/library/albums.tsx
+++ src/pages/library/albums.tsx
@@ -1,3 +1,59 @@
+import Card from "@/components/album/Card"
+import Track from "@/components/album/Track"
+import { useMusicPlayer } from "@/contexts/MusicPlayerContext"
+import getFilePath from "@/util/getFilePath"
+import useAPI from "@/util/useAPI"
+import { Button, Divider, useToast } from "@chakra-ui/react"
+import { ListEnd, ListPlus, ListStart, Minus, Play } from "lucide-react"
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
+
 export default function Albums() {
-	return <></>
+	const router = useRouter()
+	const toast = useToast()
+
+	const {
+		playAlbumAtIndex,
+		clear,
+		addQueueItem,
+		addQueueItemNext,
+		next,
+		play,
+	} = useMusicPlayer()
+
+	const [albums, setAlbums] = useState<any>()
+
+	useEffect(() => {
+		useAPI(`/user/albums`).then((albums: any) => {
+			if (albums?.data?.status == "error") {
+				router.push("/404", undefined, { shallow: true })
+				return
+			}
+
+			console.log(albums)
+
+			setAlbums(albums)
+		})
+	}, [])
+
+	return (
+		<>
+			<h1>Albums</h1>
+
+			<section>
+				{albums && Array.isArray(albums)
+					? albums?.map(function (album: any, j: number) {
+							return (
+								<Card
+									albumId={album._id}
+									albumName={album.name}
+									imageUrl={getFilePath("album", album.cover)}
+									artists={album.artists}
+								/>
+							)
+					  })
+					: ""}
+			</section>
+		</>
+	)
 }
