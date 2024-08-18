75ab440ba5b352442443acdf755cfb838f80f1d4
Lucas Täkker <lucastakker@gmail.com>
2024-08-18 15:51:58 +0200

    chore: Update next.config.mjs with NEXT_PUBLIC_API_URL environment variable

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	env: {
+		NEXT_PUBLIC_API_URL: "https://api.lucasskt.dk/",
+	},
 }
 
 export default nextConfig
