82d3d40aaef649b2ad1ae90f804d38f70356aeb6
Lucas Täkker <50625016+Sniperhund@users.noreply.github.com>
2024-08-29 08:13:37 +0000

    refactor: Update Card component to wrap album image and name in a Link tag

--- src/components/album/Card.tsx
+++ src/components/album/Card.tsx
@@ -20,20 +20,20 @@ export default function Card(props: CardProps) {
 		)
 
 	return (
-		<Link
-			href={`/album/${props.albumId}`}
-			className="text-sm justify-start flex flex-col max-w-64">
-			<img
-				src={props.imageUrl}
-				className="rounded-lg max-w-64 aspect-square"
-				alt="Album cover image"
-			/>
-			<p className="mt-0.5">{props.albumName}</p>
+		<div className="text-sm justify-start flex flex-col max-w-64">
+			<Link href={`/album/${props.albumId}`}>
+				<img
+					src={props.imageUrl}
+					className="rounded-lg max-w-64 aspect-square"
+					alt="Album cover image"
+				/>
+				<p className="mt-0.5">{props.albumName}</p>
+			</Link>
 			<ArtistName
 				artists={props.artists}
 				element="p"
 				className="opacity-50"
 			/>
-		</Link>
+		</div>
 	)
 }
