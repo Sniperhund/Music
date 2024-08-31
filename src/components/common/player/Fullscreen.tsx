de3dd701a60e300a7b15e84982b2cb637c16bdf7
Lucas Täkker <lucastakker@gmail.com>
2024-08-31 20:07:49 +0200

    refactor: Update Fullscreen component with lyrics functionality

--- src/components/common/player/Fullscreen.tsx
+++ src/components/common/player/Fullscreen.tsx
@@ -4,12 +4,41 @@ import getFilePath from "@/util/getFilePath"
 import { useContext, useEffect, useRef, useState } from "react"
 import styles from "./fullscreen.module.css"
 import Image from "next/image"
-import { X } from "lucide-react"
+import {
+	Pause,
+	Play,
+	Repeat,
+	Shuffle,
+	SkipBack,
+	SkipForward,
+	X,
+} from "lucide-react"
+import useAPI from "@/util/useAPI"
+import ArtistName from "@/components/ArtistName"
+import {
+	Slider,
+	SliderFilledTrack,
+	SliderThumb,
+	SliderTrack,
+} from "@chakra-ui/react"
 
 export default function Fullscreen() {
 	const { shown, setShown } = useContext(FullscreenContext)
 
-	const { getCurrentSong } = useMusicPlayer()
+	const {
+		isPlaying,
+		isRepeating,
+		pause,
+		play,
+		prev,
+		next,
+		getCurrentSong,
+		shuffle,
+		toggleRepeat,
+		getSecondsPlayed,
+		setSecondsPlayed,
+		currentSong,
+	} = useMusicPlayer()
 
 	function getSongAlbumUrl() {
 		if (!getCurrentSong()) return ""
@@ -20,11 +49,54 @@ export default function Fullscreen() {
 	const imageRef = useRef<HTMLImageElement>(null)
 
 	const [lyrics, setLyrics] = useState("")
+	const [syncedLyrics, setSyncedLyrics] = useState(false)
 	const [showLyrics, setShowLyrics] = useState(false)
+
+	useEffect(() => {
+		if (!currentSong) {
+			setShown(false)
+			return
+		}
+
+		useAPI(`/tracks/${currentSong._id}/lyrics`)
+			.then((result: any) => {
+				if (result?.data?.status == "error") {
+					setShowLyrics(false)
+
+					return
+				}
+
+				setLyrics(result.lyrics)
+				setSyncedLyrics(result.synced)
+
+				setShowLyrics(true)
+			})
+			.catch(() => setShowLyrics(false))
+	}, [currentSong])
+
+	const [duration, setDuration] = useState(0)
+
+	useEffect(() => {
+		if (currentSong && duration != currentSong.durationInSeconds)
+			setDuration(currentSong.durationInSeconds)
+	}, [currentSong])
+
+	const [secondsPlayed, setSecondsPlayedValue] = useState(0)
+	const [movingSlider, setMovingSlider] = useState(false)
+
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			if (!movingSlider) {
+				setSecondsPlayedValue(getSecondsPlayed())
+			}
+		}, 500)
+
+		return () => clearInterval(intervalId)
+	}, [movingSlider, getSecondsPlayed])
 
 	return (
 		<section
-			className={`fixed w-[200vw] h-[200vh] top-0 left-0 ${
+			className={`fixed w-[200vw] h-[200vh] top-0 left-0 z-50 ${
 				shown ? "block" : "hidden"
 			}`}>
 			<Image
@@ -36,26 +108,108 @@ export default function Fullscreen() {
 				height={500}
 			/>
 
-			<article className="fixed w-screen h-screen top-0 left-0 bg-black bg-opacity-25 flex justify-center items-center">
+			<section className={styles.fullscreenContainer}>
 				<X
 					className="absolute right-2 top-2 cursor-pointer"
 					size={28}
 					onClick={() => setShown(false)}
 				/>
 
-				<div
-					className={`relative ${styles.image} ${
-						showLyrics ? styles.active : ""
-					}`}>
-					<Image
-						src={getSongAlbumUrl()}
-						alt={getCurrentSong().name}
-						className="rounded-lg"
-						width={500}
-						height={500}
-					/>
-				</div>
-			</article>
+				<article className={showLyrics ? "" : styles.active}>
+					<div className={`flex flex-col gap-4`}>
+						<Image
+							src={getSongAlbumUrl()}
+							alt={getCurrentSong().name}
+							className="rounded-lg"
+							width={500}
+							height={500}
+							quality={100}
+						/>
+
+						<div>
+							<p className="font-bold text-lg">
+								{getCurrentSong().name}
+							</p>
+
+							<ArtistName
+								artists={getCurrentSong().artists}
+								element="p"
+								className="text-lg"
+							/>
+						</div>
+
+						<article>
+							<Slider
+								colorScheme="gray"
+								aria-label="slider-ex-1"
+								max={duration}
+								value={secondsPlayed}
+								focusThumbOnChange={false}
+								onChangeStart={() => {
+									setMovingSlider(true)
+								}}
+								onChange={(e) => {
+									setSecondsPlayed(e)
+									setSecondsPlayedValue(e)
+								}}
+								onChangeEnd={() => {
+									setMovingSlider(false)
+								}}>
+								<SliderTrack>
+									<SliderFilledTrack />
+								</SliderTrack>
+								<SliderThumb />
+							</Slider>
+							<div className="flex justify-between opacity-50 text-xs">
+								<p>
+									{new Date(secondsPlayed * 1000)
+										.toISOString()
+										.slice(14, 19)}
+								</p>
+								<p>
+									{new Date((duration - secondsPlayed) * 1000)
+										.toISOString()
+										.slice(14, 19)}
+								</p>
+							</div>
+						</article>
+
+						<article className="flex align-center justify-center gap-8">
+							<Shuffle size={30} onClick={() => shuffle()} />
+							<SkipBack size={30} onClick={() => prev()} />
+							{isPlaying ? (
+								<Pause
+									size={30}
+									onClick={() => {
+										pause()
+									}}
+								/>
+							) : (
+								<Play
+									size={30}
+									onClick={() => {
+										play()
+									}}
+								/>
+							)}
+							<SkipForward size={30} onClick={() => next()} />
+							<Repeat
+								size={30}
+								onClick={() => toggleRepeat()}
+								color={
+									isRepeating
+										? "rgb(144, 205, 244)"
+										: "rgb(255, 255, 255)"
+								}
+							/>
+						</article>
+					</div>
+
+					<div className={showLyrics ? styles.active : ""}>
+						<p className="text-white">{lyrics}</p>
+					</div>
+				</article>
+			</section>
 		</section>
 	)
 }
