1b3b83c2a5d20c9cd288c0ff88e1008b405c3427
Lucas Täkker <50625016+Sniperhund@users.noreply.github.com>
2024-11-05 10:48:41 +0100

    feat: Enhance PageTitle component with user sign-out functionality and create Library page

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,5 +1,55 @@
-import { Heading } from "@chakra-ui/react"
+import signout from "@/util/signout"
+import useAPI from "@/util/useAPI"
+import {
+	Avatar,
+	Flex,
+	Heading,
+	Menu,
+	MenuButton,
+	MenuItem,
+	MenuList,
+} from "@chakra-ui/react"
+import { LogOut } from "lucide-react"
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
 
 export default function PageTitle({ children }: { children: React.ReactNode }) {
-	return <Heading mb={8}>{children}</Heading>
+	const router = useRouter()
+
+	const [user, setUser] = useState<any>(null)
+
+	useEffect(() => {
+		async function fetchData() {
+			const result = await useAPI("/user")
+
+			setUser(result)
+		}
+
+		fetchData()
+	}, [])
+
+	return (
+		<div className="flex justify-between items-center w-full mb-6">
+			<Heading>{children}</Heading>
+
+			{user ? (
+				<Menu>
+					<MenuButton>
+						<Avatar name={user.name} />
+					</MenuButton>
+					<MenuList>
+						<MenuItem
+							icon={<LogOut />}
+							onClick={(e) => {
+								if (signout()) router.push("/auth/signin")
+							}}>
+							Sign out
+						</MenuItem>
+					</MenuList>
+				</Menu>
+			) : (
+				""
+			)}
+		</div>
+	)
 }
