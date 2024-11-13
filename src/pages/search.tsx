b809c477744efb58771e42429bcd56ed6ade8b16
Lucas Täkker <50625016+Sniperhund@users.noreply.github.com>
2024-11-13 11:27:05 +0100

    feat: Implement search input with debounce and routing for improved user experience

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,9 +1,11 @@
 import PageTitle from "@/components/PageTitle"
 import SearchResultCard from "@/components/SearchResultCard"
 import useAPI from "@/util/useAPI"
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 import { useDebounce } from "use-debounce"
+import { Search as SearchIcon } from "lucide-react"
 
 export default function Search() {
 	const router = useRouter()
@@ -20,9 +22,31 @@ export default function Search() {
 			.catch((err) => console.log(err))
 	}, [debouncedSearch])
 
+	const search = (e: any) => {
+		console.log(e.target.value, router.query.q)
+
+		if (e.target.value == router.query.q) return
+
+		router.push(
+			{ pathname: "/search", query: { q: e.target.value } },
+			undefined,
+			{ shallow: true },
+		)
+	}
+
 	return (
 		<>
 			<PageTitle>Search</PageTitle>
+
+			<InputGroup
+				onChange={search}
+				onFocus={search}
+				className="md:!hidden w-full">
+				<InputLeftElement pointerEvents="none">
+					<SearchIcon />
+				</InputLeftElement>
+				<Input placeholder="Search..." value={router.query.q} />
+			</InputGroup>
 
 			<section className="grid grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-4">
 				{searchResults
