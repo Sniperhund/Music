42d94a0791ffd7c78c0d466728a448222ef71ec1
Lucas Täkker <50625016+Sniperhund@users.noreply.github.com>
2025-04-01 12:57:31 +0200

    feat: Add email verification alert dialog to signup process

--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -13,15 +13,27 @@ import {
 	Heading,
 	Input,
 	Link,
+	useDisclosure,
 	useToast,
 } from "@chakra-ui/react"
 import { useFormik } from "formik"
 import { useRouter } from "next/router"
-import { ReactElement, useEffect, useState } from "react"
+import { ReactElement, useEffect, useRef, useState } from "react"
+import {
+	AlertDialog,
+	AlertDialogBody,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogContent,
+	AlertDialogOverlay,
+	AlertDialogCloseButton,
+} from "@chakra-ui/react"
 
 export default function Signup() {
 	const toast = useToast()
 	const router = useRouter()
+
+	const { isOpen, onOpen, onClose } = useDisclosure()
 
 	const formik = useFormik({
 		initialValues: {
@@ -49,7 +61,7 @@ export default function Signup() {
 				return
 			}
 
-			router.push(`/${forwardAddress || ""}`)
+			onOpen()
 		},
 	})
 
@@ -60,59 +72,92 @@ export default function Signup() {
 	}, [router.query.href])
 
 	return (
-		<Center className="h-screen">
-			<Card maxW="lg" className="w-full">
-				<CardHeader>
-					<Heading size="lg" className="text-center">
-						Create a new account!
-					</Heading>
-				</CardHeader>
-				<Divider opacity="0.2" />
-				<CardBody>
-					<form
-						onSubmit={formik.handleSubmit}
-						className="flex flex-col gap-4">
-						<FormControl isRequired>
-							<FormLabel>Full name</FormLabel>
-							<Input
-								name="name"
-								type="text"
-								value={formik.values.name}
-								onChange={formik.handleChange}
-							/>
-						</FormControl>
+		<>
+			<Center className="h-screen">
+				<Card maxW="lg" className="w-full">
+					<CardHeader>
+						<Heading size="lg" className="text-center">
+							Create a new account!
+						</Heading>
+					</CardHeader>
+					<Divider opacity="0.2" />
+					<CardBody>
+						<form
+							onSubmit={formik.handleSubmit}
+							className="flex flex-col gap-4">
+							<FormControl isRequired>
+								<FormLabel>Full name</FormLabel>
+								<Input
+									name="name"
+									type="text"
+									value={formik.values.name}
+									onChange={formik.handleChange}
+								/>
+							</FormControl>
 
-						<FormControl isRequired>
-							<FormLabel>Email address</FormLabel>
-							<Input
-								name="email"
-								type="email"
-								value={formik.values.email}
-								onChange={formik.handleChange}
-							/>
-						</FormControl>
+							<FormControl isRequired>
+								<FormLabel>Email address</FormLabel>
+								<Input
+									name="email"
+									type="email"
+									value={formik.values.email}
+									onChange={formik.handleChange}
+								/>
+							</FormControl>
 
-						<FormControl isRequired>
-							<FormLabel>Password</FormLabel>
-							<Input
-								name="password"
-								type="password"
-								value={formik.values.password}
-								onChange={formik.handleChange}
-							/>
-						</FormControl>
+							<FormControl isRequired>
+								<FormLabel>Password</FormLabel>
+								<Input
+									name="password"
+									type="password"
+									value={formik.values.password}
+									onChange={formik.handleChange}
+								/>
+							</FormControl>
 
-						<Button type="submit">Sign up</Button>
-					</form>
-				</CardBody>
-				<Divider opacity="0.2" />
-				<CardFooter>
-					<Link href={`/auth/signin?href=${forwardAddress}`}>
-						Sign in instead?
-					</Link>
-				</CardFooter>
-			</Card>
-		</Center>
+							<Button type="submit">Sign up</Button>
+						</form>
+					</CardBody>
+					<Divider opacity="0.2" />
+					<CardFooter>
+						<Link href={`/auth/signin?href=${forwardAddress}`}>
+							Sign in instead?
+						</Link>
+					</CardFooter>
+				</Card>
+			</Center>
+			<AlertDialog isOpen={isOpen} onClose={onClose}>
+				<AlertDialogOverlay>
+					<AlertDialogContent>
+						<AlertDialogHeader fontSize="lg" fontWeight="bold">
+							Verify
+						</AlertDialogHeader>
+
+						<AlertDialogBody>
+							<p>
+								An email has been sent to your email address.
+								Please verify your email address to complete the
+								signup process.
+							</p>
+							<p className="mt-2">
+								If you don't see the email, please check your
+								spam folder.
+							</p>
+						</AlertDialogBody>
+
+						<AlertDialogFooter>
+							<Button
+								onClick={() => {
+									router.push("/auth/signin")
+								}}
+								ml={3}>
+								Okay
+							</Button>
+						</AlertDialogFooter>
+					</AlertDialogContent>
+				</AlertDialogOverlay>
+			</AlertDialog>
+		</>
 	)
 }
 
