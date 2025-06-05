ee41da3fc45c189f03fbf0d987372b015ad2cc2d
Lucas Täkker <50625016+Sniperhund@users.noreply.github.com>
2025-06-05 23:01:42 +0200

    Edited the way it was typed. No features or bug fixes

--- src/components/SearchResultCard.tsx
+++ src/components/SearchResultCard.tsx
@@ -6,121 +6,115 @@ import { useMusicPlayer } from "@/contexts/MusicPlayerContext"
 import useAPI from "@/util/useAPI"
 import { Play } from "lucide-react"
 import { useLocalStorage } from "usehooks-ts"
-import EllipsisMenu from "./common/EllipsisMenu"
-import { use, useEffect } from "react"
 
 enum SearchResultCardType {
-	TRACK = "track",
-	ALBUM = "album",
-	ARTIST = "artist",
+    TRACK = "track",
+    ALBUM = "album",
+    ARTIST = "artist",
 }
 
 export interface SearchResultCardProps {
-	type: SearchResultCardType
-	id: string
-	name: string
-	imageUrl: string
-	tracks?: any
-	artists?: any
+    type: SearchResultCardType
+    id: string
+    name: string
+    imageUrl: string
+    tracks?: any
+    artists?: any
 }
 
 export default function SearchResultCard(props: SearchResultCardProps) {
-	const [recentlyPlayed, setRecentlyPlayed] = useLocalStorage<
-		SearchResultCardProps | any
-	>("recentlyPlayed", [])
+    const [recentlyPlayed, setRecentlyPlayed] = useLocalStorage<SearchResultCardProps | any>("recentlyPlayed", [])
 
-	const getLink = () => {
-		if (props.type == SearchResultCardType.ARTIST) {
-			return `/artist/${props.id}`
-		}
+    const getLink = () => {
+        if (props.type == SearchResultCardType.ARTIST) {
+            return `/artist/${props.id}`
+        }
 
-		if (props.type == SearchResultCardType.ALBUM) {
-			return `/album/${props.id}`
-		}
+        if (props.type == SearchResultCardType.ALBUM) {
+            return `/album/${props.id}`
+        }
 
-		return "#"
-	}
+        return "#"
+    }
 
-	const { addQueueItem, play: musicPlay, playAlbum, clear } = useMusicPlayer()
+    const { addQueueItem, play: musicPlay, playAlbum, clear } = useMusicPlayer()
 
-	const play = () => {
-		let _recentlyPlayed: any[] = recentlyPlayed
+    const play = () => {
+        let _recentlyPlayed: any[] = recentlyPlayed
 
-		_recentlyPlayed = [
-			props,
-			..._recentlyPlayed.filter((item: SearchResultCardProps) => {
-				return item.id != props.id
-			}),
-		]
+        _recentlyPlayed = [
+            props,
+            ..._recentlyPlayed.filter((item: SearchResultCardProps) => {
+                return item.id != props.id
+            }),
+        ]
 
-		// The amount of recently searched items to store (12 + 1)
-		if (_recentlyPlayed.length > 12) {
-			_recentlyPlayed = _recentlyPlayed.slice(0, 12)
-		}
+        // The amount of recently searched items to store (12 + 1)
+        if (_recentlyPlayed.length > 12) {
+            _recentlyPlayed = _recentlyPlayed.slice(0, 12)
+        }
 
-		setRecentlyPlayed(_recentlyPlayed)
+        setRecentlyPlayed(_recentlyPlayed)
 
-		if (props.type == SearchResultCardType.TRACK) {
-			useAPI(`/tracks/${props.id}`).then((track) => {
-				clear()
-				addQueueItem(track)
-				musicPlay()
-			})
-		}
+        if (props.type == SearchResultCardType.TRACK) {
+            useAPI(`/tracks/${props.id}`).then((track) => {
+                clear()
+                addQueueItem(track)
+                musicPlay()
+            })
+        }
 
-		if (props.type == SearchResultCardType.ALBUM) {
-			useAPI(`/albums/${props.id}/tracks`).then((tracks) => {
-				playAlbum(tracks)
-			})
-		}
+        if (props.type == SearchResultCardType.ALBUM) {
+            useAPI(`/albums/${props.id}/tracks`).then((tracks) => {
+                playAlbum(tracks)
+            })
+        }
 
-		if (props.type == SearchResultCardType.ARTIST) {
-			useAPI(`/artists/${props.id}/tracks`).then((tracks) => {
-				playAlbum(tracks)
-			})
-		}
-	}
+        if (props.type == SearchResultCardType.ARTIST) {
+            useAPI(`/artists/${props.id}/tracks`).then((tracks) => {
+                playAlbum(tracks)
+            })
+        }
+    }
 
-	return (
-		<article className="grid grid-cols-[75px_1fr] gap-4 w-full truncate">
-			<article className="relative" onClick={play}>
-				<Image
-					src={
-						props.type == SearchResultCardType.ARTIST
-							? getFilePath("artist", props.imageUrl)
-							: getFilePath("album", props.imageUrl)
-					}
-					alt={props.name}
-					width={75}
-					height={75}
-					className="rounded-lg"
-				/>
+    return (
+        <article className="grid grid-cols-[75px_1fr] gap-4 w-full truncate">
+            <article className="relative" onClick={play}>
+                <Image
+                    src={
+                        props.type == SearchResultCardType.ARTIST
+                            ? getFilePath("artist", props.imageUrl)
+                            : getFilePath("album", props.imageUrl)
+                    }
+                    alt={props.name}
+                    width={75}
+                    height={75}
+                    className="rounded-lg"
+                />
 
-				<div className="play">
-					<Play size="25px" />
-				</div>
-			</article>
+                <div className="play">
+                    <Play size="25px" />
+                </div>
+            </article>
 
-			<Link
-				href={getLink()}
-				className={`flex flex-col justify-center truncate ${
-					getLink() == "#" ? "cursor-default" : ""
-				}`}>
-				<p className="truncate">{props.name}</p>
-				<div className="opacity-50 flex">
-					<p className="capitalize">
-						{props.type != "track" ? props.type : "Song"}
-					</p>
-					{props.type != "artist" ? (
-						<p className="whitespace-pre"> - </p>
-					) : (
-						""
-					)}
-					<ArtistName artists={props.artists} element="p" />
-				</div>
-			</Link>
+            <Link
+                href={getLink()}
+                className={`flex flex-col justify-center truncate ${getLink() == "#" ? "cursor-default" : ""}`}>
+                <p className="truncate">{props.name}</p>
+                <div className="opacity-50 flex">
+                    <p className="capitalize">
+                        {props.type != "track" ? props.type : "Song"}
+                    </p>
+                    {props.type != "artist" ? (
+                        <p className="whitespace-pre"> - </p>
+                    ) : (
+                            ""
+                        )}
+                    <ArtistName artists={props.artists} element="p" />
+                </div>
+            </Link>
 
-			{/*props.type != SearchResultCardType.ARTIST && <EllipsisMenu />*/}
-		</article>
-	)
+            {/*props.type != SearchResultCardType.ARTIST && <EllipsisMenu />*/}
+        </article>
+    )
 }
